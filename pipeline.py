530520ce6eb75522e56fcc89185d69106dad4f95
tteofili <tommaso.teofili@gmail.com>
2021-02-16 08:21:35 +0100

    back to defaults

--- pipeline.py
+++ pipeline.py
@@ -151,10 +151,10 @@ if train:
         flag_Anhai = d[6]
         print(f'---{dataset_name}---')
         sigma = 3000  # generated dataset size
-        kappa = 1500  # no. of samples for consistency training
+        kappa = 1200  # no. of samples for consistency training
         epsilon = 0.015  # deviation from calculated min/max thresholds
         slicing = [0.01, 0.1, 0.15, 0.33, 0.5, 0.67, 0.75, 1]
-        num_runs = 1
+        num_runs = 3
         train_model(gt_file, t1_file, t2_file, indexes, sigma, epsilon, kappa, dataset_name, flag_Anhai, num_runs, slicing,
                     compare=True)
 if ablation:
