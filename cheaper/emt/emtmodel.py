a1de1db1d4e948edd66d808fe97bc1ea16efe3d0
Tommaso Teofili <tteofili@redhat.com>
2021-02-22 10:24:08 +0100

    eval batch = 4*batch

--- cheaper/emt/emtmodel.py
+++ cheaper/emt/emtmodel.py
@@ -84,7 +84,7 @@ class EMTERModel:
         label_list = processor.get_labels()
 
         eval_examples = processor.get_test_examples_file(testF)
-        evaluation_data_loader = load_data(eval_examples, label_list, self.tokenizer, seq_length, BATCH_SIZE,
+        evaluation_data_loader = load_data(eval_examples, label_list, self.tokenizer, seq_length, 4*BATCH_SIZE,
                                            DataType.EVALUATION, self.model_type)
 
         exp_name = 'datasets/temporary/' + dataset_name
