437ba373b512c9d51e80ae21675c4cd1b2853835
Tommaso Teofili <tteofili@redhat.com>
2021-03-01 07:59:56 +0100

    adjusted eval to match emt

--- eval.py
+++ eval.py
@@ -3,4 +3,4 @@ from pipeline import get_datasets
 
 datasets = get_datasets()
 for d in datasets:
-    cheaper_train(d, 5000, 50, 0, [0.05, 0.15, 0.33, 0.5, 1], lr=1e-4, epochs=4, compare=True, sim_length=2)
+    cheaper_train(d, 5000, 50, 0, [0.05, 0.15, 0.33, 0.5, 0.67, 1], lr=2e-5, epochs=15, compare=True, sim_length=2)
