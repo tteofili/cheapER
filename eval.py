3374ab5c3b866f90ed992d9ff5deebba96e919fa
tteofili <tommaso.teofili@gmail.com>
2021-03-08 14:27:08 +0100

    fix eval script

--- eval.py
+++ eval.py
@@ -3,5 +3,5 @@ from pipeline import get_datasets
 
 datasets = get_datasets()
 for d in datasets:
-    cheaper_train(d, 5000, 50, 0, [0.05, 0.15, 0.33, 0.5, 0.67, 1], lr=2e-5, epochs=15, compare=True, sim_length=2,
-                  models=['distilbert-base-uncased', 'roberta-base', 'squeezebert/squeezebert-mnli-headless'])
+    cheaper_train(d, 5000, 50, 0, [0.05, 0.1, 0.15, 0.2, 0.33, 0.5, 0.67, 1], lr=1e-4, epochs=15, compare=True, sim_length=5,
+                  models=['distilbert-base-uncased', 'roberta-base'])
