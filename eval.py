4f7a9a19dd8766e5774f19ec51401b1d8e54cd29
Tommaso Teofili <tteofili@redhat.com>
2021-04-29 15:30:54 +0200

    compare in eval

--- eval.py
+++ eval.py
@@ -5,6 +5,6 @@ import pandas as pd
 datasets = get_datasets()
 results = pd.DataFrame()
 for d in datasets:
-    results.append(cheaper_train(d, 3000, 1500, 0, [0.1, 0.33, 0.5, 0.7, 1], lr=2e-5, epochs=7, compare=False,
+    results.append(cheaper_train(d, 3000, 1500, 0, [0.1, 0.33, 0.5, 0.7, 1], lr=2e-5, epochs=7, compare=True,
                                  sim_length=9, models=['roberta-base'], pretrain=True, attribute_shuffle=True))
 
