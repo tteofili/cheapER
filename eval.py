457ce52280f196530243694e4c32abceab74c917
tteofili <tommaso.teofili@gmail.com>
2021-05-06 19:31:56 +0200

    minor fixes

--- eval.py
+++ eval.py
@@ -5,6 +5,6 @@ import pandas as pd
 datasets = get_datasets()
 results = pd.DataFrame()
 for d in datasets:
-    results.append(cheaper_train(d, 300, 150, 0, [0.1, 0.33, 0.5, 0.7, 1], lr=1e-5, epochs=7, compare=True, sim_length=9,
-                  models=['microsoft/deberta-base'], pretrain=True, attribute_shuffle=True, identity=True, symmetry=True))
+    results.append(cheaper_train(d, 3000, 1500, 0.15, [0.1, 0.33, 0.5, 0.7, 1], lr=2e-5, epochs=15, compare=True, sim_length=9,
+                  models=['roberta-base'], pretrain=True, attribute_shuffle=False, identity=False, symmetry=False))
 print(results)
