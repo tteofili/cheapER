2480c7cf35a8cd9863f7b51845e9122592f59492
tteofili <tommaso.teofili@gmail.com>
2021-05-12 08:14:43 +0200

    params refactoring

--- eval.py
+++ eval.py
@@ -5,18 +5,19 @@ import pandas as pd
 
 # cheapER parameters' settings
 params = CheapERParams()
-params.sigma = 300
-params.kappa = 150
+params.sigma = 3000
+params.kappa = 1500
 params.epsilon=0.05
-params.slicing=[0.1, 0.33]
+params.slicing=[0.05, 0.1, 0.2, 0.33, 0.5, 0.7, 1]
 params.lr=5e-5
-params.epochs=5
-params.pretrain=False
-params.sim_length = 5
+params.epochs=15
+params.pretrain=True
+params.sim_length = 9
 params.models = ['roberta-base']
 params.identity = False
 params.symmetry = False
 params.attribute_shuffle = False
+params.compare = True
 
 # get datasets
 datasets = get_datasets()
