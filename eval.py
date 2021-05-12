18734b225cd964fdb1c9836bab8feb8b352e1e94
tteofili <tommaso.teofili@gmail.com>
2021-05-12 09:25:26 +0200

    minor fix

--- eval.py
+++ eval.py
@@ -5,19 +5,19 @@ import pandas as pd
 
 # cheapER parameters' settings
 params = CheapERParams()
-params.sigma = 3000
-params.kappa = 1500
+params.sigma = 1000
+params.kappa = 500
 params.epsilon = 0
-params.slicing = [0.05, 0.1, 0.2, 0.33, 0.5, 0.7, 1]
+params.slicing = [0.33]
 params.lr = 5e-5
-params.epochs = 15
-params.pretrain = True
-params.sim_length = 9
-params.models = ['roberta-base']
+params.epochs = 3
+params.pretrain = False
+params.sim_length = 2
+params.models = ['distilber-base-uncased']
 params.identity = False
 params.symmetry = False
 params.attribute_shuffle = False
-params.compare = True
+params.compare = False
 
 # get datasets
 datasets = get_datasets()
