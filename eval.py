ffcbf5fb4f12d7a9c297a9edebac18d7adfcda84
tteofili <tommaso.teofili@gmail.com>
2021-05-12 09:05:36 +0200

    minor fix

--- eval.py
+++ eval.py
@@ -7,11 +7,11 @@ import pandas as pd
 params = CheapERParams()
 params.sigma = 3000
 params.kappa = 1500
-params.epsilon=0
-params.slicing=[0.05, 0.1, 0.2, 0.33, 0.5, 0.7, 1]
-params.lr=5e-5
-params.epochs=15
-params.pretrain=True
+params.epsilon = 0
+params.slicing = [0.05, 0.1, 0.2, 0.33, 0.5, 0.7, 1]
+params.lr = 5e-5
+params.epochs = 15
+params.pretrain = True
 params.sim_length = 9
 params.models = ['roberta-base']
 params.identity = False
