eade6f4bb0141a48b6880129264a40d1620cf984
Tommaso Teofili <tteofili@redhat.com>
2021-06-16 14:15:51 +0200

    added some specific scripts for single datases

--- ab.py
+++ ab.py
@@ -9,17 +9,17 @@ params.kappa = 1500
 params.epsilon = 0.15
 params.slicing = [0.05, 0.1, 0.2, 0.33, 0.4, 0.5, 0.7, 1]
 params.lr = 2e-5
-params.epochs = 7
-params.pretrain = False
+params.epochs = 15
+params.pretrain = True
 params.sim_length = 5
 params.models = ['distilbert-base-uncased', 'roberta-base']
 params.identity = False
 params.symmetry = False
 params.attribute_shuffle = False
-params.compare = True
+params.compare = False
 
 # get datasets
 datasets = get_datasets()
-results_df = cheaper_train(datasets[6], params)
+results_df = cheaper_train(datasets[4], params)
 print(results_df)
 
