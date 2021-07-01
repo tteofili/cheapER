fd0c31362fc39465458cb69a72224175745a164b
Tommaso Teofili <tteofili@redhat.com>
2021-07-01 10:59:52 +0200

    run to eval params

--- eval.py
+++ eval.py
@@ -8,18 +8,25 @@ from pipeline import get_datasets
 params = CheapERParams()
 params.sigma = 5000
 params.kappa = 100
-params.epsilon = 0.05
-params.slicing = [0.1, 0.33, 0.5, 0.7, 1]
-params.lr = 2e-5
+params.epsilon = 0.1
+params.slicing = [0.05, 0.1, 0.33, 0.5, 0.7, 1]
+params.lr = 5e-5
 params.epochs = 15
-params.pretrain = True
+params.adaptive_ft = True
 params.sim_length = 5
 params.models = ['distilbert-base-uncased', 'microsoft/deberta-base']
 params.identity = False
 params.symmetry = False
 params.attribute_shuffle = False
 params.compare = False
-
+params.deeper_trick = False
+params.consistency = True
+params.warmup = True
+params.approx = 'perceptron'
+params.balance = [0.5, 0.5]
+params.batch_size = 2
+params.sim_edges = False
+params.adjust_ds_size = False
 
 # get datasets
 datasets = get_datasets()
