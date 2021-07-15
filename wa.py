1a4f44c0ee5396d05501e20dba4de7831476efd4
tteofili <tommaso.teofili@gmail.com>
2021-07-15 13:07:13 +0200

    added wa, changed eval defaults

--- wa.py
+++ wa.py
@@ -1,5 +1,3 @@
-import pandas as pd
-
 from cheaper.params import CheapERParams
 from pipeline import cheaper_train
 from pipeline import get_datasets
@@ -31,12 +29,6 @@ params.silent = False
 
 # get datasets
 datasets = get_datasets()
-
-# perform training and collect F1 for each dataset
-results = pd.DataFrame()
-for d in datasets:
-    d_res = cheaper_train(d, params)
-    results.append(d_res)
-    print(f'{d[4]}:\n{d_res}')
-print(results)
+results_df = cheaper_train(datasets[9], params)
+print(results_df)
 
