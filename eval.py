03f8e6653d1bdbeb1b7814051f06fcc7d17bf77d
Tommaso Teofili <tteofili@redhat.com>
2021-09-17 15:50:52 +0200

    minor fixes

--- eval.py
+++ eval.py
@@ -6,21 +6,8 @@ from pipeline import get_datasets
 
 # cheapER parameters' settings
 params = CheapERParams()
-params.sigma = 3000
-params.kappa = 750
-params.epsilon = 0
 params.slicing = [0.05, 0.1, 0.2, 0.33, 0.4, 0.5, 0.7, 1]
-params.adaptive_ft = True
-params.sim_length = 5
-params.epochs = 15
 params.models = ['distilbert-base-uncased', 'microsoft/deberta-base', 'roberta-base']
-params.compare = True
-params.deeper_trick = True
-params.warmup = False
-params.batch_size = 8
-params.sim_edges = True
-params.silent = False
-params.simple_slicing = True
 
 # get datasets
 datasets = get_datasets()
