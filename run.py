46386bddace9e6e34673b74611b2b22fc6b8670b
Tommaso Teofili <tteofili@redhat.com>
2021-09-29 17:00:06 +0200

    better params?

--- run.py
+++ run.py
@@ -5,23 +5,23 @@ from pipeline import get_datasets
 # cheapER parameters' settings
 params = CheapERParams()
 params.models = ['distilbert-base-uncased']
-params.model_type = 'noisy-student'
-params.adaptive_ft = False
-params.epochs = 5
-params.deep_trick = True
-params.epsilon = 0
+params.deeper_trick = False
+params.sigma = 3000
 params.kappa = 0
-params.sigma = 20
+params.lr = 1e-5
+params.lr_multiplier = 10
+params.teaching_iterations = 5
 params.epsilon = 0
-params.sim_length = 5
+params.epochs = 7
 params.consistency = False
-params.warmup = True
+params.balance = [0.5, 0.5]
 params.batch_size = 8
+params.adaptive_ft = False
 params.compare = False
 params.silent = True
-params.lr = 1e-7
-params.lr_multiplier = 100
-params.slicing = [0.05, 0.1]
+params.model_type = 'noisy-student'
+params.data_noise = True
+
 # get datasets
 datasets = get_datasets()
 results_df = cheaper_train(datasets[5], params)
