32fdfacbfeaf382853e305700a4d2e352b54e53f
Tommaso Teofili <tteofili@redhat.com>
2021-10-11 09:18:17 +0200

    new eval, run

--- eval.py
+++ eval.py
@@ -6,8 +6,24 @@ from pipeline import get_datasets
 
 # cheapER parameters' settings
 params = CheapERParams()
-params.slicing = [0.05, 0.1, 0.2, 0.33, 0.4, 0.5, 0.7, 1]
+params.slicing = [0.05, 0.1, 0.2, 0.33, 0.4, 0.5]
 params.models = ['distilbert-base-uncased', 'microsoft/deberta-base', 'roberta-base']
+params.deeper_trick = True
+params.sigma = 100
+params.kappa = 10
+params.lr = 1e-7
+params.lr_multiplier = 200
+params.teaching_iterations = 3
+params.epsilon = 0
+params.epochs = 15
+params.consistency = True
+params.balance = [0.5, 0.5]
+params.batch_size = 8
+params.adaptive_ft = False
+params.compare = False
+params.silent = True
+params.model_type = 'noisy-student'
+params.data_noise = True
 
 # get datasets
 datasets = get_datasets()
