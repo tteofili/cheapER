28c68fd34bd2960fdf0907bc62ffeb298c0efc9c
Tommaso Teofili <tteofili@redhat.com>
2021-12-01 10:19:41 +0100

    defaults

--- cheaper/params.py
+++ cheaper/params.py
@@ -7,7 +7,7 @@ class CheapERParams:
         self.sigma = 100
         self.kappa = 10
         self.epsilon = 0.015
-        self.slicing = [0.1, 0.33, 0.5, 1]
+        self.slicing = [0.05, 0.1, 0.2, 0.4, 0.33, 0.5, 1]
         self.adaptive_ft = True
         self.num_runs = 1
         self.compare = False
