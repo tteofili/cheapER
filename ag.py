95556e39abe565e02e49d200a649f7a5e8ceb1f7
Tommaso Teofili <tteofili@redhat.com>
2021-12-17 07:59:38 +0100

    defaults

--- ag.py
+++ ag.py
@@ -4,19 +4,6 @@ from pipeline import get_datasets
 
 # cheapER parameters' settings
 params = CheapERParams()
-params.lr = 1e-6
-params.lr_multiplier = 10
-params.sigma = 100
-params.kappa = 10
-params.adjust_ds_size = False
-params.consistency = True
-params.temperature = None
-params.discard_old_data = False
-params.threshold = 0
-params.epsilon = 0
-params.label_smoothing = 0.2
-params.slicing = [0.05]
-params.epochs = 5
 
 # get datasets
 datasets = get_datasets()
